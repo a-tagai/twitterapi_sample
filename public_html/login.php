094ecf57f829666a58ea79a69a69ef0ad5d2d115
gai <tagai1106@gmail.com>
2016-02-19 01:30:09 +0900

    add bootstrap css

--- public_html/login.php
+++ public_html/login.php
@@ -7,19 +7,36 @@ require_once(__DIR__ . '/../config/config.php');
 ?>
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-	<meta charset="utf-8">
-	<title>ログイン画面</title>
-</head>
-<body>
-	<h1>ログインページ</h1>
-	<h2>通常ログイン</h2>
-	<form action="" method="post">
-		<div>
-			<div>ID: <input type="text" name="user_id" value="" placeholder="sample01"></div>
-			<div>PASS: <input type="password" name="user_password" value="" placeholder="****"></div>
-			<div><input type="submit" name="" value="ログイン"></div>
-		</div>
-	</form>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<h1 class="text-center">ログイン画面</h1>
+		<h2>通常ログイン</h2>
+		<form action="" method="post">
+			<div>
+				<div>ID: <input type="text" name="user_id" value="" placeholder="sample01"></div>
+				<div>PASS: <input type="password" name="user_password" value="" placeholder="****"></div>
+				<div><input type="submit" name="" value="ログイン"></div>
+			</div>
+		</form>
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="js/bootstrap.min.js"></script>
+	</body>
 </html>
