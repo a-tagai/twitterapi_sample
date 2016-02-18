0dff1afa53145ab7effe7a0008d51bcc2251414a
gai <tagai1106@gmail.com>
2016-02-18 23:25:15 +0900

    twitterログイン用カラム追加

--- config/init.sql
+++ config/init.sql
@@ -8,6 +8,12 @@ CREATE TABLE users (
   id int not null auto_increment primary key,
   email varchar(255) unique,
   password varchar(255),
+
+  tw_user_id bigint unique,
+  tw_user_name varchar(15),
+  tw_access_token varchar(255),
+  tw_access_token_secret varchar(255),
+
   created datetime,
   modified datetime
 );
