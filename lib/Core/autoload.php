ce709e66958acb5cdbe3d5132f109ae6dfe62b9c
gai <tagai1106@gmail.com>
2016-03-15 01:29:42 +0900

    config周り整理

--- lib/Core/autoload.php
+++ lib/Core/autoload.php
@@ -12,7 +12,7 @@ spl_autoload_register(function($class) {
   $prefix = 'App\\';
   if (strpos($class, $prefix) === 0) {
     $className = substr($class, strlen($prefix));
-    $classFilePath = __DIR__ . '/../lib/' . str_replace('\\', '/', $className) . '.php';
+    $classFilePath = __DIR__ . '/../../lib/' . str_replace('\\', '/', $className) . '.php';
     if (file_exists($classFilePath)) {
       require $classFilePath;
     }
---
+++ lib/Core/head.php
@@ -0,0 +1,8 @@
+<?php
+require_once(__DIR__ . '/autoload.php');
+require_once(__DIR__ . '/../../vendor/autoload.php');
+require_once(__DIR__ . '/../functions.php');
+
+require_once(__DIR__ . '/../../config/config.php');
+
+session_start();
